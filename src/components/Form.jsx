778c7401ffb5b3ba4f61f037cc8c1571d0b49312
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-21 11:08:14 +0530

    add controlled form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CustomInput from './common/CustomInput'
 import CustomButton from './common/CustomButton'
 
 const Form = () => {
+    const initialValue = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    }
+    const [FormValue, setFormValue] = useState(initialValue);
     return (
         <>
             <div className="flex min-h-screen items-center justify-center">
                 <div className="max-w-[1140px] w-full px-4 mt-4">
                     <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
-                        <CustomInput type="text" placeholder="First Name" />
-                        <CustomInput type="text" placeholder="Last Name" />
+                        <CustomInput
+                            name="firstName"
+                            value={FormValue.firstName}
+                            type="text"
+                            placeholder="First Name"
+                            onchange={(e) => setFormValue({ ...FormValue, firstName: e.target.value })} />
+                        <CustomInput
+                            name="lastName"
+                            value={FormValue.lastName}
+                            type="text"
+                            placeholder="Last Name"
+                            onchange={(e) => setFormValue({ ...FormValue, lastName: e.target.value })}
+                        />
                     </div>
-                    <CustomInput inputClass="sm:max-w-[816px] mx-auto flex mt-4" type="email" placeholder="Email" />
+                    <CustomInput
+                        inputClass="sm:max-w-[816px] mx-auto flex mt-4"
+                        name="email"
+                        value={FormValue.email}
+                        type="email"
+                        placeholder="Email"
+                        onchange={(e) => setFormValue({ ...FormValue, email: e.target.value })} />
                     <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
-                        <CustomInput type="Password" placeholder="Password" />
-                        <CustomInput type="Password" placeholder="Confirm Password" />
+                        <CustomInput
+                            name="password"
+                            value={FormValue.password}
+                            type="Password"
+                            placeholder="Password"
+                            onchange={(e) => setFormValue({ ...FormValue, password: e.target.value })} />
+                        <CustomInput
+                            name="confirmPassword"
+                            value={FormValue.confirmPassword}
+                            type="Password"
+                            placeholder="Confirm Password"
+                            onchange={(e) => setFormValue({ ...FormValue, confirmPassword: e.target.value })}
+                        />
                     </div>
                     <CustomButton type="Submit" text="Submit" />
                 </div>
--- src/components/common/CustomButton.jsx
+++ src/components/common/CustomButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CustomButton = ({ text, type }) => {
+const CustomButton = ({ name, text, type, onchange, value }) => {
     return (
-        <button type={type} className="px-7 rounded-4xl font-mono flex cursor-pointer mx-auto py-2 border-[1.5px] mt-6 border-black transition-all duration-500 ease-in-out hover:text-white hover:[box-shadow:inset_0_-100px_0_0_black]">
+        <button name={name} value={value} type={type} onChange={onchange} className="px-7 rounded-4xl font-mono flex cursor-pointer mx-auto py-2 border-[1.5px] mt-6 border-black transition-all duration-500 ease-in-out hover:text-white hover:[box-shadow:inset_0_-100px_0_0_black]">
             {text}
         </button>
     )
