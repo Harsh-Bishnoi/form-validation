705850cac32495aa122e96cd969dd28341d53951
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-22 14:30:08 +0530

    change in code

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ name, text, type, onchange, value }) => {
     return (
-        <button name={name} value={value} type={type} onChange={onchange} className="px-7 rounded-4xl font-mono flex cursor-pointer mx-auto py-2 border-[1.5px] mt-6 border-black transition-all duration-500 ease-in-out hover:text-white hover:[box-shadow:inset_0_-100px_0_0_black]">
+        <button name={name} value={value} type={type} onChange={onchange} className="px-7 rounded-4xl font-mono flex cursor-pointer mx-auto py-2 border-[1.5px] mt-6 border-black transition-all duration-500 ease-in-out hover:text-white hover:[box-shadow:inset_109px_0_0_0_black]">
             {text}
         </button>
     )
